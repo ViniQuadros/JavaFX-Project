d2989e44bc60433272aafddc833199bfbd202f4c
Vinícius de Quadros <127548304+ViniQuadros@users.noreply.github.com>
2024-08-27 20:07:11 -0300

    Adding Variables

---
+++ fxproject/src/main/java/javafxproject/ListController.java
@@ -0,0 +1,27 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class ListController {
+
+    @FXML
+    private Button btnAdd;
+
+    @FXML
+    private Button btnDelete;
+
+    @FXML
+    private TextField txtSearch;
+
+    @FXML
+    void addListItem(ActionEvent event) {
+
+    }
+
+    @FXML
+    void deleteListItem(ActionEvent event) {
+
+    }
+
+}
